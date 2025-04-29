62bb6c0b44e2deaa8b453b36082159543e1965dc
mohammad aden ferangga <feranggaaden@gmail.com>
2025-04-29 13:24:16 +0700

    Update delete.php

--- Kasir/delete.php
+++ Kasir/delete.php
@@ -6,9 +6,9 @@ if (isset($_GET['id'])) {
     $query = "DELETE FROM kasir WHERE id = $id";
 
     if (mysqli_query($conn, $query)) {
-        echo "<script>alert('Data berhasil dihapus!'); window.location.href='konfirmasi.php';</script>";
+        echo "<script>alert('Data berhasil dihapus!'); window.location.href='Konfirmasi.php';</script>";
     } else {
-        echo "<script>alert('Gagal menghapus data!'); window.location.href='konfirmasi.php';</script>";
+        echo "<script>alert('Gagal menghapus data!'); window.location.href='Konfirmasi.php';</script>";
     }
 } else {
     // Jika parameter id tidak ada, kembali ke konfirmasi.php
